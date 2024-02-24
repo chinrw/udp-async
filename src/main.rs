837b114eb2c45175ecac42609fdac0f8ad0cfce4
Ruowen Qin <chinqrw@gmail.com>
2024-02-24 10:15:39 +0800

    Use another tasks to record recv
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::sync::Arc;
 use std::time::{Duration, Instant};
 use tokio::net::UdpSocket;
 use tokio::runtime::Handle;
@@ -14,42 +15,51 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (tx, mut rx) = mpsc::channel::<()>(num_tasks);
 
     let start = Instant::now();
+    let mut tasks = vec![];
     for _ in 0..num_tasks {
         let tx = tx.clone();
         let server_address = server_address.to_string();
 
-        task::spawn(async move {
+        let task = task::spawn(async move {
             let mut sockets = Vec::new();
             for _ in 0..sockets_per_task {
                 let socket = UdpSocket::bind("0.0.0.0:0").await.unwrap();
-                sockets.push(socket);
+                sockets.push(Arc::new(socket));
             }
-            let metrics = Handle::current().metrics();
-
-            let n = metrics.num_workers();
-            println!("Runtime is using {} workers", n);
 
             for i in 0..send_numbers / num_tasks / sockets_per_task {
                 for socket in &sockets {
                     let message = format!("Hello, World! {}", i);
+                    let socket = Arc::clone(&socket);
                     socket
                         .send_to(message.as_bytes(), &server_address)
                         .await
                         .unwrap();
-                    let mut buffer = [0; 1024];
-                    let (len, _) = socket.recv_from(&mut buffer).await.unwrap();
-                    println!("Received: {}", String::from_utf8_lossy(&buffer[..len]));
+                    task::spawn(async move {
+                        let mut buffer = [0; 1024];
+                        let (len, _) = socket.recv_from(&mut buffer).await.unwrap();
+                        // println!("Received: {}", String::from_utf8_lossy(&buffer[..len]));
+                    })
+                    .await
+                    .unwrap();
                 }
             }
-
-            drop(sockets); // Explicitly drop sockets if needed
-            let _ = tx.send(()).await;
+            // drop(sockets); // Explicitly drop sockets if needed
+            // let _ = tx.send(()).await;
         });
+        tasks.push(task);
+    }
+    for task in tasks {
+        let metrics = Handle::current().metrics();
+
+        let n = metrics.active_tasks_count();
+        println!("Runtime has {} active tasks", n);
+        task.await.unwrap();
     }
 
-    for _ in 0..num_tasks {
-        rx.recv().await;
-    }
+    // for _ in 0..num_tasks {
+    //     rx.recv().await;
+    // }
     let duration = start.elapsed();
 
     println!("Time elapsed in expensive_function() is: {:?}", duration);
